d50621e39be4dda72a6fa834865b34868595a80b
Nana Fujishima <n.fujishima.it@gmail.com>
2025-01-31 21:28:04 +0900

    add deploy.yml

---
+++ .vitepress/config.ts
@@ -0,0 +1,10 @@
+export default {
+  // site-level options
+  title: 'VitePress',
+  description: 'Just playing around.',
+  base: "/vitepress-blog/",
+
+  themeConfig: {
+    // theme-level options
+  }
+}
