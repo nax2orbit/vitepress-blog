35ba90f46b470b8457d7af518c31a442631d57dc
Nana Fujishima <n.fujishima.it@gmail.com>
2025-02-15 22:14:05 +0900

    css参照パスを修正

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "My Vitepress blog",
   description: "A VitePress Site",
+  base: "/vitepress-blog/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
