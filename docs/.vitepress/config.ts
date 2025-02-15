7305684e5ccfcdea7d538b6a7d6865ea4e50f651
Nana Fujishima <n.fujishima.it@gmail.com>
2025-02-15 22:51:55 +0900

    ドメイン更新

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'vitepress'
 export default defineConfig({
   title: "My Vitepress blog",
   description: "A VitePress Site",
-  base: "/vitepress-blog/",
+  base: "/",
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
