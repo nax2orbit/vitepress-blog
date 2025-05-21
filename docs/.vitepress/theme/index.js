fded66cb8d4133773929ec68d3ac8fb5ca84ef02
Nana Fujishima <n.fujishima.it@gmail.com>
2025-05-21 22:13:14 +0900

    til index

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,10 +1,12 @@
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
 import DocsList from './components/DocsList.vue'
+import TilList from './components/TilList.vue'
 
 export default {
   ...DefaultTheme,
   enhanceApp({ app }) {
     app.component('DocsList', DocsList)
+    app.component('TilList', TilList)
   }
 }
